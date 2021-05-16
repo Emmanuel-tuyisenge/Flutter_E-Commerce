0f5043ad0f300b38bfdfd8186e8b4f0c7e2dbc07
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-05-16 16:39:15 +0200

    Create home_Page

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Wilders";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("E-Commerce App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello $days $name !"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
