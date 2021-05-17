fdec0878ec84caaf4ecea8ecda43b548a9ead4ad
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-05-17 15:41:08 +0200

    create a drawer

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl =
+        "https://lh3.googleusercontent.com/proxy/8MXxTyYm0j2zHuDR1msEaW0aJ3teEVY3XU-ohLHine3snpgo9azJFsOOBra65EpEf9S2S6jGUlVhxt67bXZ1JwVgA_YmkWozzv_5EHNI_C00dLS0wo2L31enaTuor4gP9N2ZuCr9d16F";
+
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                //decoration: BoxDecoration(color: Colors.red),
+                accountName: Text("Manu Wilder"),
+                accountEmail: Text("lilcoolmanu@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
